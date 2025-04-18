7156e84b34b21e404bae82765d30370f85620100
priestleydj <priestleydj@gmail.com>
2025-04-18 01:24:22 +0100

    Update app with all sections and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './sections/Home.jsx'
+import { useState } from "react"
+import "./App.css"
+import Navbar from "./components/Navbar.jsx"
+import Home from "./sections/Home.jsx"
+import AboutMe from "./sections/AboutMe.jsx"
+import Projects from "./sections/Projects.jsx"
+import ContactMe from "./sections/ContactMe.jsx"
 
 function App() {
 
   return (
-    <Home />
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <Home />
+      <AboutMe />
+      <Projects />
+      <ContactMe />
+    </div >
   )
 }
 
